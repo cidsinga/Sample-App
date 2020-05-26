a9e6fa1d2e9abc082e124b8ad570a65b0e7cca20
cidsinga <cidsinga@gmail.comm>
2020-05-26 14:31:51 -0700

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "hellow world!"
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
+  root 'static_pages#home'
+  get 'contact/home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  root 'application#hello'
+  get 'static_pages/contact'
 end
