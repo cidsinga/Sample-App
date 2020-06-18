85d966769fd2f2573340c28cc25d937d29460b6a
cidsinga <cidsinga@gmail.comm>
2020-06-18 09:09:21 -0700

    add test fixes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -10,6 +11,5 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get contact_path
-    assert_select "title", full_title("Contact")
   end
 end
