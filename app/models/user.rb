c738072a45bf8f011739709337a8d076b2b6e70a
cidsinga <cidsinga@gmail.comm>
2020-07-13 09:48:29 -0700

    add update action to password reset controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,6 +71,9 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+
   private
 
     # Converts email to all lower-case.
