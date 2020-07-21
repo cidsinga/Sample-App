6fe9618122151f746eb699954b054daa23ff1f2b
cidsinga <cidsinga@gmail.comm>
2020-07-21 15:31:12 -0700

    add correction to sendgrid spelling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => Rails.application.credentials.dig(:sendgid, :username),
-    :password       => Rails.application.credentials.dig(:sendgid, :password),
+    :user_name      => Rails.application.credentials.dig(:sendgrid, :username),
+    :password       => Rails.application.credentials.dig(:sendgrid, :password),
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
